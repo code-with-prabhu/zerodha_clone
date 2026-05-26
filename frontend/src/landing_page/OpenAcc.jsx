d2eaa4a98f8299b95c0b7397d8e6571b1ef2abe4
code-with-prabhu <prabhudattarout2005@gmail.com>
2026-05-26 10:55:46 +0530

    Completed Routing setup for all the links

--- frontend/src/landing_page/OpenAcc.jsx
+++ frontend/src/landing_page/OpenAcc.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 const OpenAcc = () => {
   return (
-    <div>
-      
-    </div>
+    <div className="container mx-auto p-12 m-5">
+      <div className='text-center flex flex-col items-center'>
+            <h1 className='mt-5 text-gray-600 text-3xl font-semibold'>Open a Zerodha account</h1>
+
+            <p className="text-gray-600 mt-2 text-2xl mb-4">Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
+
+            <button className="p-2 bg-blue-500 hover:bg-blue-700 text-white text-lg rounded-md mb-5 w-1/5 mx-auto transition mt-5">Sign up for free</button>
+          </div>
+      </div>
   )
 }
 
--- frontend/src/landing_page/home/Hero.jsx
+++ frontend/src/landing_page/home/Hero.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <div className="container mx-auto p-5 h-screen">
+    <div className="container mx-auto p-12 m-5">
       <div className='text-center flex flex-col items-center'>
-            <img src="/images/HomeHero.svg" alt="Home Image" className='mb-5'/>
+            <img src="/images/HomeHero.svg" alt="Home Image" className='mb-5 max-h-[65vh]'/>
             <h1 className='mt-5 text-gray-600 text-3xl font-semibold'>Invest in everything</h1>
 
-            <p className="text-gray-600 mt-2 text-2xl">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
+            <p className="text-gray-600 mt-2 text-2xl mb-4">Online platform to invest in stocks, derivatives, mutual funds, ETFs, bonds, and more.</p>
 
-            <button className="p-2 bg-blue-500 hover:bg-blue-700 text-white text-lg rounded-md mb-5 w-1/5 mx-auto transition mt-2">Sign up for free</button>
+            <button className="p-2 bg-blue-500 hover:bg-blue-700 text-white text-lg rounded-md mb-5 w-1/5 mx-auto transition mt-5">Sign up for free</button>
           </div>
       </div>
   )
